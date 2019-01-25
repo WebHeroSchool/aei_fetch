558f0ffcc05939ab3bc5d82c251a84b2e7698be9
aei <ashmee90@gmail.com>
2019-01-25 23:45:30 +0500

    fix script, add catch null

--- main.js
+++ main.js
@@ -1,70 +1,38 @@
-// let url = 'https://api.github.com/users/';
-// let user = window.location;
-// let urlGit = url + user;
-// let imgUrl = '';
-// let userName = '';
-// let profileDescription = '';
-//
-// fetch(urlGit)
-//     .then(response => response.json())
-//     .then(function(data) {
-//         console.log(data);
-//         imgUrl = data.avatar_url;
-//         userName = data.name;
-//         profileDescription = data.bio;
-//
-//     })
-//     .catch(err => console.log('Информация о пользователе не доступна'));
-//
-// const getAvatar = function(imgUrl){
-//
-//     const gitName = document.createTextNode(userName);
-//     document.body.appendChild(gitName);
-//
-//     const gitAvatar =  document.createElement("img");
-//     gitAvatar.setAttribute('alt','аватарка');
-//     gitAvatar.setAttribute('src',imgUrl);
-//     document.body.appendChild(gitAvatar);
-//
-//     const gitBio = document.createTextNode(profileDescription);
-//     document.body.appendChild(gitBio);
-// }
-//
-
-//let urlGit = url + user;
+let url = 'https://api.github.com/users/';
+let user = window.location.search.slice(10,);
+let urlGit = url + user;
 let imgUrl = '';
 let userName = '';
 let profileDescription = '';
 
-// Ник пользователя нужно получить из параметра в URL.
-//     по маске из задания                 {адрес страницы}?username={имя пользователя}
-//берем имя из введенного адреса "?username=6thSence"
-let nameFromUrl = window.location.search.slice(10,);   //возвратить должно "6thSence"
+fetch(urlGit)
+    .then(response => response.json())
+    .then(function(data) {
+        console.log(data);
+        imgUrl = data.avatar_url;
+        if (imgUrl != null) {
+            const gitAvatar =  document.createElement("img");
+            gitAvatar.setAttribute('alt','аватарка');
+            gitAvatar.setAttribute('src',imgUrl);
+            document.body.appendChild(gitAvatar);
+        } else
+            console.log('Информация об аватарке пользователе не доступна');
 
-function getUserInfo(nameFromUrl) {
-    return fetch('https://api.github.com/users/' + name)
-        .then(response => response.json())
-        .then(function(data) {
-            console.log(data);
-            imgUrl = data.avatar_url;
-            userName = data.name;
-            profileDescription = data.bio;
-        })
-        .catch(err => console.log('Информация о пользователе не доступна'));
+        userName = data.name;
+        if (userName != null) {
+            const gitName = document.createElement("h1");
+            document.body.appendChild(gitName);
+            gitName.appendChild(document.createTextNode(userName));
 
-}
-getUserInfo(nameFromUrl);
+        } else
+            console.log('Информация об имени пользователя не доступна');
 
-const makeBlocks = function(){
-    const gitName = document.createTextNode(userName);
-    document.body.appendChild(gitName);
-    const gitAvatar =  document.createElement("img");
-    gitAvatar.setAttribute('alt','аватарка');
-    gitAvatar.setAttribute('src',imgUrl);
-    document.body.appendChild(gitAvatar);
-
-    const gitBio = document.createTextNode(profileDescription);
-    document.body.appendChild(gitBio);
-};
-
-makeBlocks();
+        profileDescription = data.bio;
+        if (profileDescription != null) {
+            const gitBio = document.createElement("div");
+            document.body.appendChild(gitBio);
+            gitBio.appendChild(document.createTextNode(profileDescription));
+        } else
+            console.log('Информация об описании профиля пользователя не доступна');
+    })
+    .catch(err => console.log('Информация о пользователе не доступна'));
